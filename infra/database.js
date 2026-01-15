0ff10806ebbb789f4dc03029a20d2326e3b2fddc
João Pedro Guimarães <joaopedro.enoki@gmail.com>
2026-01-15 11:41:16 -0300

    fix: handle `client` as `undefined` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
   let client;
@@ -7,10 +7,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.log("\n Erro dentro do catch do database.js:");
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
@@ -39,5 +40,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === 'production' ? true : false;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
